0bba08c8e00fd1550d885f68d6963a3ac2314f6d
Arthur Geisweiller <50842954+geisweiller@users.noreply.github.com>
2020-10-20 02:01:05 -0300

    Expo init

---
+++ App.tsx
@@ -0,0 +1,20 @@
+
+import React from 'react';
+
+import Header from './src/components/Header';
+import Footer from './src/components/Footer';
+import Products from './src/screens/Products';
+
+export default function App() {
+  return (
+
+    <>
+     <Header/>
+     <Products/>
+     <Footer/>
+    </>
+   
+  );
+}
+
+
