7ed6f95a9011f073f0eb7b2806e906a9ee09461a
Arthur Geisweiller <50842954+geisweiller@users.noreply.github.com>
2020-10-20 02:01:49 -0300

    Color config

---
+++ src/styles/ColorScheme.ts
@@ -0,0 +1,61 @@
+export const colorScheme = (productTypeId : number) => {
+
+let color;
+
+switch (productTypeId) {
+  case 1:   
+    color = '#008DCB';
+    break;
+
+  case 2:   
+    color = '#D5A82C';
+    break;
+
+  case 3:   
+    color = '#94E5D2';
+    break;
+
+  case 4:   
+    color = '#86B2DE';
+    break;
+
+  case 5:   
+    color = '#5AAAD5';
+    break;
+
+  case 6:   
+    color = '#38BFA0';
+    break;
+
+  case 7:   
+    color = '#3E71B9';
+    break;
+
+  case 8:   
+    color = '#4C309B';
+    break;
+
+  case 9:   
+    color = '#86B2DE';
+    break;
+
+  case 10:   
+    color = '#B9B42C';
+    break;
+
+  case 11:   
+    color = '#4141D5';
+    break;
+
+  case 12:   
+    color = '#9390E5';
+    break;
+
+}
+
+return color;
+};
+
+
+
+ 
