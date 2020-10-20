524abce7865adf749a689fcc23bef41008aacd8f
Arthur Geisweiller <50842954+geisweiller@users.noreply.github.com>
2020-10-20 17:37:42 -0300

    Unit Tests

---
+++ __tests__/App.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Header from '../src/components/Header';
+import Footer from '../src/components/Footer';
+import Products from '../src/screens/Product'
+
+it('Products Rendered', () => {
+  renderer.create(<Products />);
+});
+
+it('Footer Rendered', () => {
+  renderer.create(<Footer />);
+});
+
+it('Header Rendered', () => {
+  renderer.create(<Header />);
+});
