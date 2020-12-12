0895792a00de6efbeee7d350db1921bd9dfc4c1e
Alexandre Junior <juniordias_@live.com>
2020-12-12 17:53:54 -0300

    chore: Screen Options

--- kinvo/src/pages/Actions/index.tsx
+++ kinvo/src/pages/Actions/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { View } from 'react-native';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 const Actions: React.FC = () => {
-  return <View />;
+  return <Container />;
 };
 
 export default Actions;
--- kinvo/src/routes/index.tsx
+++ kinvo/src/routes/index.tsx
@@ -3,6 +3,8 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import { Actions, Home } from '../pages';
 
+import Left from '../assets/svgs/left.svg';
+
 const Challange = createStackNavigator();
 
 const ChallangeRoutes: React.FC = () => (
@@ -22,7 +24,26 @@ const ChallangeRoutes: React.FC = () => (
     }}
   >
     <Challange.Screen name="Desafio" component={Home} />
-    <Challange.Screen name="Ações" component={Actions} />
+    <Challange.Screen
+      options={({ navigation }) => ({
+        headerTitleStyle: {
+          fontFamily: 'Montserrat',
+          fontWeight: 'bold',
+          fontSize: 20,
+          marginTop: -20,
+          marginLeft: -20,
+        },
+
+        headerLeftContainerStyle: {
+          margin: 15,
+          marginBottom: 30,
+          marginLeft: 15,
+        },
+        headerLeft: () => <Left onPress={() => navigation.goBack()} />,
+      })}
+      name="Ações"
+      component={Actions}
+    />
   </Challange.Navigator>
 );
 
