3828b3816f52dd862afff9091f9f08156960643f
Guibson <guibson@Guibsons-MacBook-Pro.local>
2020-12-13 01:24:24 -0300

    Create Desafio page

--- src/pages/Desafio/index.tsx
+++ src/pages/Desafio/index.tsx
@@ -1,9 +1,64 @@
 import React from 'react';
+import {useNavigation} from '@react-navigation/native';
 
-import {Container} from './styles';
+import AppBar from '../../components/AppBar';
+
+import {
+  Container,
+  Content,
+  Card,
+  Leading,
+  Body,
+  Title,
+  Subtitle,
+  Badge,
+  BadgeContent,
+} from './styles';
+
+import Acoes from '../../assets/svgs/acoes.svg';
+import Fundos from '../../assets/svgs/fundos.svg';
+import Previdencias from '../../assets/svgs/previdencias.svg';
 
 const Desafio: React.FC = () => {
-  return <Container />;
+  const {navigate} = useNavigation();
+
+  return (
+    <Container>
+      <AppBar title="Desafio" />
+      <Content>
+        <Card onPress={() => navigate('Acoes')}>
+          <Leading>
+            <Acoes />
+          </Leading>
+          <Body>
+            <Title>Ações</Title>
+            <Subtitle>Nacionais</Subtitle>
+          </Body>
+        </Card>
+        <Card onPress={() => navigate('Fundos')}>
+          <Leading>
+            <Fundos />
+          </Leading>
+          <Body>
+            <Title>Fundos</Title>
+            <Subtitle>De investimentos</Subtitle>
+          </Body>
+          <Badge>
+            <BadgeContent>Novo</BadgeContent>
+          </Badge>
+        </Card>
+        <Card onPress={() => navigate('Previdencias')}>
+          <Leading>
+            <Previdencias />
+          </Leading>
+          <Body>
+            <Title>Previdências</Title>
+            <Subtitle>Privadas</Subtitle>
+          </Body>
+        </Card>
+      </Content>
+    </Container>
+  );
 };
 
 export default Desafio;
--- src/pages/Desafio/styles.ts
+++ src/pages/Desafio/styles.ts
@@ -1,3 +1,63 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const Content = styled.ScrollView`
+  flex: 1;
+  padding: 20px;
+`;
+
+export const Card = styled.TouchableOpacity.attrs({
+  activeOpacity: 0.6,
+})`
+  background-color: ${({theme}) => theme.colors.white};
+  padding: 32px 20px;
+  border-radius: 10px;
+  border-width: 1px;
+  border-color: ${({theme}) => theme.colors.grayBorder};
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+export const Leading = styled.View`
+  width: 56px;
+  height: 56px;
+  border-radius: 28px;
+  background-color: ${({theme}) => theme.colors.grayBackground};
+  margin-right: 12px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Body = styled.View`
+  flex: 1;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  font-family: ${({theme}) => theme.fonts.bold};
+  font-size: 16px;
+  color: ${({theme}) => theme.colors.primary};
+  margin-bottom: 5px;
+`;
+
+export const Subtitle = styled.Text`
+  font-family: ${({theme}) => theme.fonts.semiBold};
+  font-size: 10px;
+  color: ${({theme}) => theme.colors.grayText};
+`;
+
+export const Badge = styled.View`
+  background-color: ${({theme}) => theme.colors.blueNew};
+  padding: 4px 15px;
+  border-radius: 10px;
+`;
+
+export const BadgeContent = styled.Text`
+  font-family: ${({theme}) => theme.fonts.medium};
+  font-size: 10px;
+  color: ${({theme}) => theme.colors.white};
+`;
