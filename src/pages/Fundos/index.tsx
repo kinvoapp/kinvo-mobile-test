4109a89d5decc8c73ba4b02562b3c61a89b96c3b
Guibson <guibson@Guibsons-MacBook-Pro.local>
2020-12-13 23:43:07 -0300

    Create Fundos page

--- src/pages/Fundos/index.tsx
+++ src/pages/Fundos/index.tsx
@@ -1,9 +1,75 @@
 import React from 'react';
+import AppBar from '../../components/AppBar';
 
-import {Container} from './styles';
+import {
+  Container,
+  List,
+  Card,
+  Header,
+  Info,
+  Title,
+  Subtitle,
+  Badge,
+  BadgeContent,
+  Row,
+  Key,
+  Value,
+  ValueText,
+} from './styles';
+
+import ArrowDown from '../../assets/svgs/arrow-down.svg';
+import StarIcon from '../../assets/svgs/star.svg';
+import FillStarIcon from '../../assets/svgs/star-fill.svg';
+
+const data = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const Fundos: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <AppBar title="Fundos" hasBack />
+      <List
+        data={data}
+        keyExtractor={(item, index) => String(index)}
+        renderItem={({item, index}) => (
+          <Card>
+            <Header>
+              <Info>
+                <Title>Alaska Black</Title>
+                <Subtitle>MULTIMERCADOS</Subtitle>
+              </Info>
+              <Badge>
+                <BadgeContent>Novo</BadgeContent>
+              </Badge>
+            </Header>
+            <Row>
+              <Key>Classificação:</Key>
+              <Value>
+                {Array.from({length: 3}, (_, i) => 0 + i).map((e) => (
+                  <FillStarIcon key={e} />
+                ))}
+                {Array.from({length: 5 - 3}, (_, i) => 0 + i).map((e) => (
+                  <StarIcon key={e} />
+                ))}
+              </Value>
+            </Row>
+            <Row>
+              <Key>Valor mínimo:</Key>
+              <Value>
+                <ValueText>R$ 24,17</ValueText>
+              </Value>
+            </Row>
+            <Row>
+              <Key>Rentabilidade:</Key>
+              <Value>
+                <ArrowDown />
+                <ValueText orange>-27%</ValueText>
+              </Value>
+            </Row>
+          </Card>
+        )}
+      />
+    </Container>
+  );
 };
 
 export default Fundos;
--- src/pages/Fundos/styles.ts
+++ src/pages/Fundos/styles.ts
@@ -1,3 +1,86 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const List = styled.FlatList`
+  flex: 1;
+  padding: 20px;
+`;
+
+export const Card = styled.View`
+  border-radius: 10px;
+  border-width: 1px;
+  border-color: ${({theme}) => theme.colors.grayBorder};
+  padding: 20px 20px 5px 20px;
+  margin-bottom: 20px;
+  background-color: ${({theme}) => theme.colors.white};
+`;
+
+export const Header = styled.View`
+  flex-direction: row;
+  border-bottom-width: 1px;
+  border-bottom-color: ${({theme}) => theme.colors.grayBorder};
+  margin-bottom: 10px;
+`;
+
+export const Info = styled.View`
+  flex: 1;
+`;
+
+export const Title = styled.Text`
+  font-family: ${({theme}) => theme.fonts.bold};
+  font-size: 16px;
+  color: ${({theme}) => theme.colors.grayText};
+  margin-bottom: 3px;
+`;
+
+export const Subtitle = styled.Text`
+  font-family: ${({theme}) => theme.fonts.semiBold};
+  font-size: 12px;
+  color: ${({theme}) => theme.colors.grayText};
+  margin-bottom: 15px;
+`;
+
+export const Badge = styled.View`
+  padding: 4px 16px;
+  background-color: ${({theme}) => theme.colors.blueNew};
+  border-radius: 10px;
+  align-self: flex-start;
+`;
+
+export const BadgeContent = styled.Text`
+  font-family: ${({theme}) => theme.fonts.medium};
+  font-size: 10px;
+  color: ${({theme}) => theme.colors.white};
+`;
+
+export const Row = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 15px;
+`;
+
+export const Key = styled.Text`
+  font-family: ${({theme}) => theme.fonts.medium};
+  font-size: 10px;
+  color: ${({theme}) => theme.colors.grayText};
+`;
+
+export const Value = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+interface ValueTextProps {
+  orange?: boolean;
+}
+
+export const ValueText = styled.Text<ValueTextProps>`
+  font-family: ${({theme}) => theme.fonts.semiBold};
+  font-size: 12px;
+  color: ${({theme, orange = false}) =>
+    orange ? theme.colors.orangeProfit : theme.colors.grayText};
+`;
