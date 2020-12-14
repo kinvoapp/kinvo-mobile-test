69bae6ac4e741c36039f484a0abe74b3a1d9b165
Guibson <guibson@Guibsons-MacBook-Pro.local>
2020-12-13 23:42:48 -0300

    Create Ações page

--- src/pages/Acoes/index.tsx
+++ src/pages/Acoes/index.tsx
@@ -1,9 +1,63 @@
 import React from 'react';
+import AppBar from '../../components/AppBar';
 
-import {Container} from './styles';
+import {
+  Container,
+  List,
+  Card,
+  Header,
+  Info,
+  Title,
+  Subtitle,
+  Like,
+  Row,
+  Key,
+  Value,
+  ValueText,
+} from './styles';
+
+import ArrowDown from '../../assets/svgs/arrow-down.svg';
+import FillLikeIcon from '../../assets/svgs/like-fill.svg';
+import LikeIcon from '../../assets/svgs/like.svg';
+
+const data = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const Acoes: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <AppBar title="Ações" hasBack />
+      <List
+        data={data}
+        keyExtractor={(item, index) => String(index)}
+        renderItem={({item, index}) => (
+          <Card>
+            <Header>
+              <Info>
+                <Title>Magazine Luiza</Title>
+                <Subtitle>MGLU3</Subtitle>
+              </Info>
+              <Like>
+                <LikeIcon />
+              </Like>
+            </Header>
+            <Row>
+              <Key>Valor mínimo:</Key>
+              <Value>
+                <ValueText>R$ 24,17</ValueText>
+              </Value>
+            </Row>
+            <Row>
+              <Key>Rentabilidade:</Key>
+              <Value>
+                <ArrowDown />
+                <ValueText orange>-27%</ValueText>
+              </Value>
+            </Row>
+          </Card>
+        )}
+      />
+    </Container>
+  );
 };
 
 export default Acoes;
