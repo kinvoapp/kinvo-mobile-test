1996986312081ea3e98be3280565e6a335abf789
Guibson <guibson@Guibsons-MacBook-Pro.local>
2020-12-13 23:44:59 -0300

    Create Previdências page

--- src/pages/Previdencias/index.tsx
+++ src/pages/Previdencias/index.tsx
@@ -1,9 +1,94 @@
-import React from 'react';
+import React, {useState, useCallback} from 'react';
+import AppBar from '../../components/AppBar';
 
-import {Container} from './styles';
+import {
+  Container,
+  List,
+  Card,
+  Header,
+  Info,
+  Title,
+  Subtitle,
+  Row,
+  Key,
+  Value,
+  ValueText,
+  Filters,
+  FilterGap,
+  Filter,
+  FilterContent,
+} from './styles';
+
+import ArrowDown from '../../assets/svgs/arrow-down.svg';
+
+const data = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const Previdencias: React.FC = () => {
-  return <Container />;
+  const [filter, setFilter] = useState(1);
+
+  const handleFilterChange = useCallback((filterNumber) => {
+    setFilter(filterNumber);
+  }, []);
+
+  return (
+    <Container>
+      <AppBar title="Previdências" hasBack />
+      <List
+        ListHeaderComponent={() => (
+          <Filters>
+            <Filter onPress={() => handleFilterChange(1)} active={filter === 1}>
+              <FilterContent active={filter === 1}>SEM TAXA</FilterContent>
+            </Filter>
+            <FilterGap />
+            <Filter onPress={() => handleFilterChange(2)} active={filter === 2}>
+              <FilterContent active={filter === 2}>R$ 100,00</FilterContent>
+            </Filter>
+            <FilterGap />
+            <Filter onPress={() => handleFilterChange(3)} active={filter === 3}>
+              <FilterContent active={filter === 3}>D+1</FilterContent>
+            </Filter>
+          </Filters>
+        )}
+        data={data}
+        keyExtractor={(item, index) => String(index)}
+        renderItem={({item, index}) => (
+          <Card>
+            <Header>
+              <Info>
+                <Title>Alaska Prev</Title>
+                <Subtitle>MULTIMERCADOS</Subtitle>
+              </Info>
+            </Header>
+            <Row>
+              <Key>Valor mínimo:</Key>
+              <Value>
+                <ValueText>R$ 1.000,00</ValueText>
+              </Value>
+            </Row>
+            <Row>
+              <Key>Taxa:</Key>
+              <Value>
+                <ValueText>2,00%</ValueText>
+              </Value>
+            </Row>
+            <Row>
+              <Key>Resgate:</Key>
+              <Value>
+                <ValueText>D+ 10</ValueText>
+              </Value>
+            </Row>
+            <Row>
+              <Key>Rentabilidade:</Key>
+              <Value>
+                <ArrowDown />
+                <ValueText orange>-27,50%</ValueText>
+              </Value>
+            </Row>
+          </Card>
+        )}
+      />
+    </Container>
+  );
 };
 
 export default Previdencias;
--- src/pages/Previdencias/styles.ts
+++ src/pages/Previdencias/styles.ts
@@ -1,3 +1,106 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const List = styled.FlatList`
+  flex: 1;
+  padding: 20px;
+`;
+
+export const Filters = styled.View`
+  flex-direction: row;
+  align-items: center;
+  padding-bottom: 20px;
+  border-bottom-width: 1px;
+  border-bottom-color: ${({theme}) => theme.colors.grayBorder};
+  margin-bottom: 20px;
+`;
+
+interface FilterProps {
+  active?: boolean;
+}
+
+export const Filter = styled.TouchableOpacity<FilterProps>`
+  background-color: ${({theme, active = false}) =>
+    active ? theme.colors.primary : theme.colors.white};
+  border-radius: 100px;
+  padding: 8px 16px;
+  align-items: center;
+  flex: 1;
+`;
+
+export const FilterGap = styled.View`
+  width: 20px;
+`;
+
+export const FilterContent = styled.Text<FilterProps>`
+  font-family: ${({theme}) => theme.fonts.semiBold};
+  font-size: 12px;
+  color: ${({theme, active = false}) =>
+    active ? theme.colors.white : theme.colors.grayText};
+`;
+
+export const Card = styled.View`
+  border-radius: 10px;
+  border-width: 1px;
+  border-color: ${({theme}) => theme.colors.grayBorder};
+  padding: 20px 20px 5px 20px;
+  margin-bottom: 20px;
+  background-color: ${({theme}) => theme.colors.white};
+`;
+
+export const Header = styled.View`
+  flex-direction: row;
+  border-bottom-width: 1px;
+  border-bottom-color: ${({theme}) => theme.colors.grayBorder};
+  margin-bottom: 10px;
+`;
+
+export const Info = styled.View`
+  flex: 1;
+`;
+
+export const Title = styled.Text`
+  font-family: ${({theme}) => theme.fonts.bold};
+  font-size: 16px;
+  color: ${({theme}) => theme.colors.grayText};
+  margin-bottom: 3px;
+`;
+
+export const Subtitle = styled.Text`
+  font-family: ${({theme}) => theme.fonts.semiBold};
+  font-size: 12px;
+  color: ${({theme}) => theme.colors.grayText};
+  margin-bottom: 15px;
+`;
+
+export const Row = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 15px;
+`;
+
+export const Key = styled.Text`
+  font-family: ${({theme}) => theme.fonts.medium};
+  font-size: 10px;
+  color: ${({theme}) => theme.colors.grayText};
+`;
+
+export const Value = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+interface ValueTextProps {
+  orange?: boolean;
+}
+
+export const ValueText = styled.Text<ValueTextProps>`
+  font-family: ${({theme}) => theme.fonts.semiBold};
+  font-size: 12px;
+  color: ${({theme, orange = false}) =>
+    orange ? theme.colors.orangeProfit : theme.colors.grayText};
+`;
