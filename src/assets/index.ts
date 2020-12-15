12f44f2584c7a5169f7ae4dbbcc9d2a5be78d437
hiagopsilva <hiagohps360@gmail.com>
2020-12-14 23:18:25 -0300

    feat: assets

---
+++ src/assets/index.ts
@@ -0,0 +1,10 @@
+// Icons
+export { default as Actions } from './actions.png';
+export { default as Foresight } from './foresight.png';
+export { default as HeartEmpty } from './heartEmpty.png';
+export { default as HeartFull } from './heartFull.png';
+export { default as StartDefaultEmpty } from './startDefaultEmpty.png';
+export { default as StartDefaultFull } from './startDefaultFull.png';
+export { default as StarFullSecondary } from './starFullSecondary.png';
+export { default as StartEmptySecondary } from './StartEmptySecondary.png';
+export { default as Wallet } from './Wallet.png';
