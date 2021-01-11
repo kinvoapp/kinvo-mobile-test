b66de8221d246aab3316477f36a152d2e12eb302
mollinari1 <77169940+mollinari1@users.noreply.github.com>
2021-01-11 00:45:46 -0300

    Add files via upload

---
+++ appkinvo/Detail.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { View, Text} from 'react-native';
+import api from '../pages/api';
+
+class Detail extends Component{
+async ComponentDidMount()
+{
+    const response= await.api.get('')
+
+<ScrollView>
+<Fundos
+cover={require('../assets/tela-fundos.png')}
+onPress={() => navigation.navigate('detail')}
+/>
+
+
+</ScrollView>
+
+    render(){
+     return(
+
+     );
+
+    }
+
+
+}}
+
+export default function Detail()
