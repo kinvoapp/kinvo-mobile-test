09a0118071564b3a31e49b2adbd1b6a7a0fc7802
Pablo Henrique <pablohenrique.r.c@gmail.com>
2021-03-12 22:15:48 -0300

    Sets basic project structure
    Adds files of strings, colors and some assets.

---
+++ kinvo/src/util/colors.js
@@ -0,0 +1,14 @@
+const colors = {
+    primary: '#6F4DBF',
+    background: '#E4E4E4',
+    blue: '#36C4D6',
+    gray: '#818181',
+    badgeText: '#F7F8F8',
+    text: '#627179',
+    yellow: '#F8C22E',
+    red: '#E85D1F',
+    green: '#AED335',
+    white: '#FFFFFF'
+}
+
+export default colors
---
+++ kinvo/src/util/strings.js
@@ -0,0 +1,21 @@
+const strings = {
+    challenge: "Desafio",
+    national: "Nacionais",
+    shares: "Ações",
+    investiment: "De investimentos",
+    funds: "Fundos",
+    private: "Privadas",
+    pensions: "Previdências",
+    new: "Novo",
+    closed: "Fechado",
+    minimumValue: "Valor mínimo",
+    profitability: "Rentabilidade",
+    rating: "Classificação",
+    tax: "Taxa",
+    redemptionTerm: "Resgate",
+    pensionFilter1: "SEM TAXA",
+    pensionFilter2: "R$ 100,00",
+    pensionFilter3: "D+1"
+}
+
+export default strings
