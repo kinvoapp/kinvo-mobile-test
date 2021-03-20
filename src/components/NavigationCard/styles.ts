b15b167e1de40dd8755c3a691ca4dae0aa1f5207
Lucas Borges <lscborges1@gmail.com>
2021-03-20 12:27:56 -0300

    Added header + navigation cards

---
+++ src/components/NavigationCard/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  width: 80%;
+  height: 120px;
+  background-color: #fff;
+  border-radius: 10px;
+  margin-top: 20px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const NavigationImageContainer = styled.View``;
+
+export const NavigationImage = styled.Image`
+  height: 29px;
+  width: 29px;
+`;
