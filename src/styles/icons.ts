669a58278ac166a04fbde3a3a722fa45dbf4e9ff
luisescx <luis.esculapio@live.com>
2022-04-29 18:33:41 -0300

    feat: added icons on styles

---
+++ src/styles/icons.ts
@@ -0,0 +1,19 @@
+import FilledHeart from '~/assets/icons/filled_heart.svg';
+import FilledStar from '~/assets/icons/filled_star.svg';
+import Funds from '~/assets/icons/funds.svg';
+import Pension from '~/assets/icons/pension.svg';
+import Stocks from '~/assets/icons/stocks.svg';
+import UnfilledHeart from '~/assets/icons/unfilled_heart.svg';
+import UnfilledStar from '~/assets/icons/unfilled_star.svg';
+
+const icons = {
+  filledHeart: FilledHeart,
+  filledStar: FilledStar,
+  funds: Funds,
+  pension: Pension,
+  stocks: Stocks,
+  unfilledHeart: UnfilledHeart,
+  unfilledStar: UnfilledStar,
+};
+
+export default icons;
