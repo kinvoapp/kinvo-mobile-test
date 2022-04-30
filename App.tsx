1dc3968f4a88ebb5393f7c32ee00da7ed555b54d
luisescx <luis.esculapio@live.com>
2022-04-30 11:27:32 -0300

    feat: added contexts on app.tsx

--- App.tsx
+++ App.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
-import { ThemeProvider } from 'styled-components/native';
-import theme from '~/styles/themes';
+import Contexts from '~/contexts';
 import Navigation from '~/navigations';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <StatusBar barStyle="dark-content" backgroundColor={theme.colors.white} />
-
+    <Contexts>
       <Navigation />
-    </ThemeProvider>
+    </Contexts>
   );
 }
 
