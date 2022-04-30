1271805bed7faa04c0551e13e0587908db6062b1
luisescx <luis.esculapio@live.com>
2022-04-30 20:44:24 -0300

    feat: ios splash screen icons, app icons and apps name

--- src/navigations/index.tsx
+++ src/navigations/index.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import SplashScreen from 'react-native-splash-screen';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigation from './AppNavigation';
 
 function Navigation() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <NavigationContainer>
       <AppNavigation />
