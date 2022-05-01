12bab4624aab3cad477d47ea5b5d853d4d47aa43
luisescx <luis.esculapio@live.com>
2022-05-01 10:40:19 -0300

    refactor: added animation with reanimated on loading component

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,14 +1,53 @@
-import React from 'react';
-import { ActivityIndicator } from 'react-native';
-import { useTheme } from 'styled-components/native';
+import React, { useEffect, useRef } from 'react';
+import Animated, {
+  Easing,
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withTiming,
+} from 'react-native-reanimated';
+import LoadingImage from '~/assets/images/load.png';
+import { ReactNodeProps } from '~/types/reactNode';
 import { Container } from './styles';
 
-const Loading = () => {
-  const { colors } = useTheme();
+interface LoadingProps extends ReactNodeProps {
+  visible: boolean;
+}
+
+const Loading: React.FC<LoadingProps> = ({ visible, children }) => {
+  const wasStarted = useRef(false);
+
+  const loadingAnim = useSharedValue(0);
+
+  const imageStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          rotateZ: `${loadingAnim.value}deg`,
+        },
+      ],
+    };
+  });
+
+  useEffect(() => {
+    if (visible && !wasStarted.current) {
+      wasStarted.current = true;
+
+      loadingAnim.value = withRepeat(
+        withTiming(360, { duration: 750, easing: Easing.linear }),
+        -1,
+      );
+    }
+  }, [visible, loadingAnim]);
+
+  if (!visible) {
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    return <>{children}</>;
+  }
 
   return (
     <Container>
-      <ActivityIndicator size="large" color={colors.primary} />
+      <Animated.Image style={imageStyle} source={LoadingImage} />
     </Container>
   );
 };
