bdf2d8f259cf78ff3609e59ab65b7d6d111e4560
lucaspedronet <lucaspedrolbg@gmail.com>
2022-08-10 18:28:59 -0300

    feat: novo componente DivisorInLine

---
+++ src/components/DivisorInLine/DivisorInLine.styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components/native';
+import { View } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { DivisorInLineProps } from './DivisorInLine';
+
+export const Container = styled(View) <Pick<DivisorInLineProps, 'isPadding'>>`
+  flex: 1;
+
+  padding: 0 ${({ theme, isPadding }) => theme.size.padding[isPadding]}px;
+
+  background-color: transparent;
+`;
+
+export const Line = styled(View)`
+  height: ${RFValue(1)}px;
+
+  background-color: ${({ theme }) => theme.colors.border.grayLight};
+`;
