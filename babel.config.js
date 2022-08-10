eac70877881b723422afe48c0bdd18c44e35cc60
lucaspedronet <lucaspedrolbg@gmail.com>
2022-08-10 10:26:10 -0300

    feat: configuração da estrutura de pastas, prettier e svg

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,26 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@__mocks__': './src/__mocks__',
+          '~@types': './src/@types',
+          '~assets/icons': './src/assets/icons',
+          '~assets/images': './src/assets/images',
+          '~constants': './src/constants',
+          '~contexts': './src/contexts',
+          '~components': './src/components',
+          '~config': './src/config',
+          '~hooks': './src/hooks',
+          '~routes': './src/routes',
+          '~screens/public': './src/screens/public',
+          '~types': './src/types',
+          '~services': './src/services',
+          '~utils': './src/utils',
+        },
+      },
+    ],
+  ],
 };
