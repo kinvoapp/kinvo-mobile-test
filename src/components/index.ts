8a40b46e4696d61ae0e6c78863ed492d2c4d23dc
lucaspedronet <lucaspedrolbg@gmail.com>
2022-08-12 17:10:42 -0300

    Feat: novo component <ButtonSimple />

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,8 @@ export * from './Header/Header';
 export * from './DivisorInLine/DivisorInLine';
 export * from './CardBase/CardBase';
 export * from './CardAction/CardAction';
+export * from './ErrorNetwork/ErrorNetwork';
+export * from './RenderCondition/RenderCondition';
+export * from './ButtonSimple/ButtonSimple';
+export * from './CardPension/CardPension';
+export * from './Slogan/Slogan';
